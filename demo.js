85b2a4cab61fd3bacc5c70c829f3664c343c922c
Anton Ivanov <an.ivan0209@gmail.com>
2016-02-07 03:12:09 +0200

    add/remove/check

--- demo.js
+++ demo.js
@@ -3,7 +3,8 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      // Show newest tasks at the top
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 
@@ -22,4 +23,17 @@ if (Meteor.isClient) {
       event.target.text.value = "";
     }
   });
+
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+          $set: {checked: ! this.checked}
+        });
+      },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+
+});
 }
